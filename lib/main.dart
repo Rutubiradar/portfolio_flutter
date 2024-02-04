c1981e0b133c7d5578e164665dc1d596c14019d6
Rutubiradar <98686743+Rutubiradar@users.noreply.github.com>
2024-02-04 12:57:15 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Portfolio',
+      title: 'Rutuja Birdar',
       theme: ThemeData(
         primarySwatch: Colors.purple,
         canvasColor: Colors.purple.shade50,
